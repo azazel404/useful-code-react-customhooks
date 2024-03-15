c9dc401c26cc60101a6045bf6e7751bbdc2f9a24
hendra <gunawan4041@gmail.com>
2024-03-15 14:26:37 +0700

    changes name project

--- index.js
+++ index.js
@@ -4,10 +4,4 @@ import useDebounce from './src/useDebounce'
 import useTimeout from './src/useTimeout'
 import useToggle from './src/useToggle'
 
-export default {
- useToggle,
- useArray,
- useDebounce,
- useTimeout,
- useToggle,
-}
+export { useToggle, useArray, useDebounce, useTimeout }
