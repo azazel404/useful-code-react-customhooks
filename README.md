4e07585e3540fb26d4796dfd847bf0f57921fde6
hendra <gunawan4041@gmail.com>
2024-03-15 14:02:19 +0700

    add custom hooks react

---
+++ README.md
@@ -0,0 +1,13 @@
+# Javascript React Mini Custom Hook
+
+A simple package for managing reactions on Discord messages.
+
+## Installation
+
+You can install the package using npm:
+
+```
+npm install mini-react-custom-hook
+```
+
+## Usage
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import useToggle from './useToggle'
+import useArray from './useArray'
+import useDebounce from './useDebounce'
+import useTimeout from './useTimeout'
+import useToggle from './useToggle'
+
+export default {
+ useToggle,
+ useArray,
+ useDebounce,
+ useTimeout,
+ useToggle,
+}
