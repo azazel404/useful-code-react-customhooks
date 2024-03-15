a095a965071bcd660f79393d2506fe0ba2c0a072
hendra <gunawan4041@gmail.com>
2024-03-15 14:06:30 +0700

    update package json

---
+++ index.js
@@ -0,0 +1,13 @@
+import useToggle from './src/useToggle'
+import useArray from './src/useArray'
+import useDebounce from './src/useDebounce'
+import useTimeout from './src/useTimeout'
+import useToggle from './src/useToggle'
+
+export default {
+ useToggle,
+ useArray,
+ useDebounce,
+ useTimeout,
+ useToggle,
+}
